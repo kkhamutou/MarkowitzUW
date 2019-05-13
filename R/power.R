290c9c1d74a70acb71f6c67688a04e09e95d46fd
Kiryl Khamutou <k.khamutou@gmail.com>
2019-05-13 22:08:51 +0200

    Init upload

---
+++ R/power.R
@@ -0,0 +1,25 @@
+#' Square a number
+#'
+#' Take in any numeric value and square it
+#' @param x A numeric value to be squared
+#' @return The square of the input
+#' @export
+squared <- function(x){
+  return(x^2)
+}
+
+#' Cube a number
+#' @param x Number to be cubed
+#' @return The cube of the input
+#' @export
+cube <- function(x) {
+  return(x^3)
+}
+
+#' Quadrat a number
+#' @param x Number to be quadrated
+#' @return The quadrad of the input
+#' @export
+quadratic <- function(x){
+  return(x^4)
+}
